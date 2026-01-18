310bf0676ff8a39d6bdb5fda79fa4c982fba9527
Omar Williams <willio4@unlv.nevada.edu>
2026-01-18 00:56:27 -0800

    Add new articles and images for the latest features
    - Added images for Bad Bunny, Concrete, Drake & Brent, Dreamt DE, Griselda, SZA Summer, and Yeat.
    - Created new EJS views for articles: concrete.ejs, drakebrent.ejs, dreamtde.ejs, griselda.ejs, szasummer.ejs, and yeat.ejs.
    - Updated main.css to enhance styling for article headers, buttons, and carousel items.
    - Modified index.ejs to include a section for displaying the latest articles with links to read more.

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ import express from "express";
 import ejs from "ejs";
 
 let articles = [];
+const article = {};
 
 const app = express();
 const port = 3000;
+
+function getTime() {
+    const month = new Date().getMonth() + 1;
+    const day = new Date().getDate();
+    const year = new Date().getFullYear();
+    return `${month}/${day}/${year}`;
+}
 
 // Set EJS as the view engine
 app.set("view engine", "ejs");
@@ -18,10 +26,104 @@ app.use(express.static("public"));
 
 // Routes
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", {
+    articles: articles,
+  });
 });
+
+app.get("/drakebrent", (req, res) => {
+  res.render("drakebrent.ejs", {
+    articles: articles,
+  });
+});
+
+app.get("/concrete", (req, res) => {
+  res.render("concrete.ejs", {
+    articles: articles,
+  });
+});
+
+app.get("/dreamtde", (req, res) => {
+  res.render("dreamtde.ejs", {
+    articles: articles,
+  });
+});
+
+app.get("/yeat", (req, res) => {
+  res.render("yeat.ejs", {
+    articles: articles,
+  });
+});
+
+app.get("/szasummer", (req, res) => {
+  res.render("szasummer.ejs", {
+    articles: articles,
+  });
+});
+
+app.get("/griselda", (req, res) => {
+  res.render("griselda.ejs", {
+    articles: articles,
+  });
+});
+
+
+
+
 
 // Start server
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+// creation of articles
+articles = [
+    {
+        title: "Drake, Brent Faiyaz, and the Blur Between Rap and R&B",
+        author: "Omar Williams" ,
+        content: "Phasellus feugiat sapien ex, eu bibendum dui lacinia nec. Vestibulum semper hendrerit diam, eu vestibulum eros euismod quis. Nam id urna sed leo dictum accumsan. In mollis, risus et malesuada sagittis, lacus orci cursus felis, sit amet blandit odio urna at purus. Phasellus laoreet, libero sed maximus faucibus, sapien lorem condimentum odio, vitae consectetur justo nisi ut lacus. Morbi viverra, velit et volutpat varius, tortor nisl bibendum nibh, vel sagittis felis lectus eget nibh. <br><br>Maecenas pellentesque mauris sit amet mollis placerat. Nunc finibus malesuada dolor, nec facilisis ante volutpat vitae. Vivamus in odio lacus. Cras turpis arcu, pretium vel turpis pulvinar, consectetur scelerisque quam. Suspendisse sed quam ac ligula porttitor mattis. Quisque auctor ligula eleifend, laoreet enim vitae, varius nisl. Quisque nec nibh eleifend, pulvinar turpis eget, fringilla justo. <br><br>Duis aliquet arcu venenatis, dignissim justo id, lobortis tellus. Nam hendrerit tellus ut nulla tempor, sed congue orci elementum. Ut vitae justo porttitor orci lobortis auctor eget sit amet lorem. Aliquam lobortis efficitur cursus. Etiam non dui massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. <br><br>Praesent posuere egestas velit sit amet molestie. Nulla facilisi. Fusce porta, est sed scelerisque ultricies, quam sem pharetra nibh, vitae ultricies sapien nisl in mauris. In at nunc et sem lobortis consectetur. Nam volutpat vitae dolor vitae gravida. Nullam vitae aliquam orci, vitae sagittis felis. Duis pellentesque hendrerit felis, blandit iaculis dolor maximus sit amet. Nullam congue malesuada massa, ac lobortis dui placerat sit amet. Duis eu maximus enim. Integer blandit risus justo, vel tincidunt arcu cursus vel. <br><br>Donec dictum ac purus sed suscipit. Sed ultrices nunc eget metus pellentesque elementum. Fusce consectetur sagittis nulla, vitae rhoncus libero mattis nec. Maecenas posuere finibus tincidunt. Aenean nulla diam, feugiat vitae dolor eu, finibus vulputate metus.",
+        date: getTime(),
+        img: "/images/drakebrent.jpg",
+        url: "/drakebrent",
+    },
+    {
+        title: "Opium, Concrete Boys, and the New Sound of Chaos",
+        author: "Omar Williams",
+        content: "Phasellus feugiat sapien ex, eu bibendum dui lacinia nec. Vestibulum semper hendrerit diam, eu vestibulum eros euismod quis. Nam id urna sed leo dictum accumsan. In mollis, risus et malesuada sagittis, lacus orci cursus felis, sit amet blandit odio urna at purus. Phasellus laoreet, libero sed maximus faucibus, sapien lorem condimentum odio, vitae consectetur justo nisi ut lacus. Morbi viverra, velit et volutpat varius, tortor nisl bibendum nibh, vel sagittis felis lectus eget nibh. <br><br>Maecenas pellentesque mauris sit amet mollis placerat. Nunc finibus malesuada dolor, nec facilisis ante volutpat vitae. Vivamus in odio lacus. Cras turpis arcu, pretium vel turpis pulvinar, consectetur scelerisque quam. Suspendisse sed quam ac ligula porttitor mattis. Quisque auctor ligula eleifend, laoreet enim vitae, varius nisl. Quisque nec nibh eleifend, pulvinar turpis eget, fringilla justo. <br><br>Duis aliquet arcu venenatis, dignissim justo id, lobortis tellus. Nam hendrerit tellus ut nulla tempor, sed congue orci elementum. Ut vitae justo porttitor orci lobortis auctor eget sit amet lorem. Aliquam lobortis efficitur cursus. Etiam non dui massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. <br><br>Praesent posuere egestas velit sit amet molestie. Nulla facilisi. Fusce porta, est sed scelerisque ultricies, quam sem pharetra nibh, vitae ultricies sapien nisl in mauris. In at nunc et sem lobortis consectetur. Nam volutpat vitae dolor vitae gravida. Nullam vitae aliquam orci, vitae sagittis felis. Duis pellentesque hendrerit felis, blandit iaculis dolor maximus sit amet. Nullam congue malesuada massa, ac lobortis dui placerat sit amet. Duis eu maximus enim. Integer blandit risus justo, vel tincidunt arcu cursus vel. <br><br>Donec dictum ac purus sed suscipit. Sed ultrices nunc eget metus pellentesque elementum. Fusce consectetur sagittis nulla, vitae rhoncus libero mattis nec. Maecenas posuere finibus tincidunt. Aenean nulla diam, feugiat vitae dolor eu, finibus vulputate metus.",
+        date: getTime(),
+        img: "/images/concrete.jpeg",
+        url: "/concrete",
+    },
+    {
+        title: "From TDE to Dreamville: Who’s Really Moving the Culture Now",
+        author: "Omar Williams",
+        content: "Phasellus feugiat sapien ex, eu bibendum dui lacinia nec. Vestibulum semper hendrerit diam, eu vestibulum eros euismod quis. Nam id urna sed leo dictum accumsan. In mollis, risus et malesuada sagittis, lacus orci cursus felis, sit amet blandit odio urna at purus. Phasellus laoreet, libero sed maximus faucibus, sapien lorem condimentum odio, vitae consectetur justo nisi ut lacus. Morbi viverra, velit et volutpat varius, tortor nisl bibendum nibh, vel sagittis felis lectus eget nibh. <br><br>Maecenas pellentesque mauris sit amet mollis placerat. Nunc finibus malesuada dolor, nec facilisis ante volutpat vitae. Vivamus in odio lacus. Cras turpis arcu, pretium vel turpis pulvinar, consectetur scelerisque quam. Suspendisse sed quam ac ligula porttitor mattis. Quisque auctor ligula eleifend, laoreet enim vitae, varius nisl. Quisque nec nibh eleifend, pulvinar turpis eget, fringilla justo. <br><br>Duis aliquet arcu venenatis, dignissim justo id, lobortis tellus. Nam hendrerit tellus ut nulla tempor, sed congue orci elementum. Ut vitae justo porttitor orci lobortis auctor eget sit amet lorem. Aliquam lobortis efficitur cursus. Etiam non dui massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. <br><br>Praesent posuere egestas velit sit amet molestie. Nulla facilisi. Fusce porta, est sed scelerisque ultricies, quam sem pharetra nibh, vitae ultricies sapien nisl in mauris. In at nunc et sem lobortis consectetur. Nam volutpat vitae dolor vitae gravida. Nullam vitae aliquam orci, vitae sagittis felis. Duis pellentesque hendrerit felis, blandit iaculis dolor maximus sit amet. Nullam congue malesuada massa, ac lobortis dui placerat sit amet. Duis eu maximus enim. Integer blandit risus justo, vel tincidunt arcu cursus vel. <br><br>Donec dictum ac purus sed suscipit. Sed ultrices nunc eget metus pellentesque elementum. Fusce consectetur sagittis nulla, vitae rhoncus libero mattis nec. Maecenas posuere finibus tincidunt. Aenean nulla diam, feugiat vitae dolor eu, finibus vulputate metus.",
+        date: getTime(),
+        img: "/images/dreamtde.jpeg",
+        url: "/dreamtde",
+    },
+    {
+        title: "Yeat, Ken Carson, and Why Energy Matters More Than Lyrics Right Now",
+        author: "Omar Williams",
+        content: "Phasellus feugiat sapien ex, eu bibendum dui lacinia nec. Vestibulum semper hendrerit diam, eu vestibulum eros euismod quis. Nam id urna sed leo dictum accumsan. In mollis, risus et malesuada sagittis, lacus orci cursus felis, sit amet blandit odio urna at purus. Phasellus laoreet, libero sed maximus faucibus, sapien lorem condimentum odio, vitae consectetur justo nisi ut lacus. Morbi viverra, velit et volutpat varius, tortor nisl bibendum nibh, vel sagittis felis lectus eget nibh. <br><br>Maecenas pellentesque mauris sit amet mollis placerat. Nunc finibus malesuada dolor, nec facilisis ante volutpat vitae. Vivamus in odio lacus. Cras turpis arcu, pretium vel turpis pulvinar, consectetur scelerisque quam. Suspendisse sed quam ac ligula porttitor mattis. Quisque auctor ligula eleifend, laoreet enim vitae, varius nisl. Quisque nec nibh eleifend, pulvinar turpis eget, fringilla justo. <br><br>Duis aliquet arcu venenatis, dignissim justo id, lobortis tellus. Nam hendrerit tellus ut nulla tempor, sed congue orci elementum. Ut vitae justo porttitor orci lobortis auctor eget sit amet lorem. Aliquam lobortis efficitur cursus. Etiam non dui massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. <br><br>Praesent posuere egestas velit sit amet molestie. Nulla facilisi. Fusce porta, est sed scelerisque ultricies, quam sem pharetra nibh, vitae ultricies sapien nisl in mauris. In at nunc et sem lobortis consectetur. Nam volutpat vitae dolor vitae gravida. Nullam vitae aliquam orci, vitae sagittis felis. Duis pellentesque hendrerit felis, blandit iaculis dolor maximus sit amet. Nullam congue malesuada massa, ac lobortis dui placerat sit amet. Duis eu maximus enim. Integer blandit risus justo, vel tincidunt arcu cursus vel. <br><br>Donec dictum ac purus sed suscipit. Sed ultrices nunc eget metus pellentesque elementum. Fusce consectetur sagittis nulla, vitae rhoncus libero mattis nec. Maecenas posuere finibus tincidunt. Aenean nulla diam, feugiat vitae dolor eu, finibus vulputate metus.",
+        date: getTime(),
+        img: "/images/yeat.jpeg",
+        url: "/yeat",
+    },
+    {
+        title: "SZA, Summer Walker, and the Era of Emotional Minimalism",
+        author: "Omar Williams",
+        content: "Phasellus feugiat sapien ex, eu bibendum dui lacinia nec. Vestibulum semper hendrerit diam, eu vestibulum eros euismod quis. Nam id urna sed leo dictum accumsan. In mollis, risus et malesuada sagittis, lacus orci cursus felis, sit amet blandit odio urna at purus. Phasellus laoreet, libero sed maximus faucibus, sapien lorem condimentum odio, vitae consectetur justo nisi ut lacus. Morbi viverra, velit et volutpat varius, tortor nisl bibendum nibh, vel sagittis felis lectus eget nibh. <br><br>Maecenas pellentesque mauris sit amet mollis placerat. Nunc finibus malesuada dolor, nec facilisis ante volutpat vitae. Vivamus in odio lacus. Cras turpis arcu, pretium vel turpis pulvinar, consectetur scelerisque quam. Suspendisse sed quam ac ligula porttitor mattis. Quisque auctor ligula eleifend, laoreet enim vitae, varius nisl. Quisque nec nibh eleifend, pulvinar turpis eget, fringilla justo. <br><br>Duis aliquet arcu venenatis, dignissim justo id, lobortis tellus. Nam hendrerit tellus ut nulla tempor, sed congue orci elementum. Ut vitae justo porttitor orci lobortis auctor eget sit amet lorem. Aliquam lobortis efficitur cursus. Etiam non dui massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. <br><br>Praesent posuere egestas velit sit amet molestie. Nulla facilisi. Fusce porta, est sed scelerisque ultricies, quam sem pharetra nibh, vitae ultricies sapien nisl in mauris. In at nunc et sem lobortis consectetur. Nam volutpat vitae dolor vitae gravida. Nullam vitae aliquam orci, vitae sagittis felis. Duis pellentesque hendrerit felis, blandit iaculis dolor maximus sit amet. Nullam congue malesuada massa, ac lobortis dui placerat sit amet. Duis eu maximus enim. Integer blandit risus justo, vel tincidunt arcu cursus vel. <br><br>Donec dictum ac purus sed suscipit. Sed ultrices nunc eget metus pellentesque elementum. Fusce consectetur sagittis nulla, vitae rhoncus libero mattis nec. Maecenas posuere finibus tincidunt. Aenean nulla diam, feugiat vitae dolor eu, finibus vulputate metus.",
+        date: getTime(),
+        img: "/images/szasummer.jpg",
+        url: "/szasummer",
+    },
+    {
+        title: "Griselda, Freddie Gibbs, and the Return of Raw Rap",
+        author: "Omar Williams",
+        content: "Phasellus feugiat sapien ex, eu bibendum dui lacinia nec. Vestibulum semper hendrerit diam, eu vestibulum eros euismod quis. Nam id urna sed leo dictum accumsan. In mollis, risus et malesuada sagittis, lacus orci cursus felis, sit amet blandit odio urna at purus. Phasellus laoreet, libero sed maximus faucibus, sapien lorem condimentum odio, vitae consectetur justo nisi ut lacus. Morbi viverra, velit et volutpat varius, tortor nisl bibendum nibh, vel sagittis felis lectus eget nibh. <br><br>Maecenas pellentesque mauris sit amet mollis placerat. Nunc finibus malesuada dolor, nec facilisis ante volutpat vitae. Vivamus in odio lacus. Cras turpis arcu, pretium vel turpis pulvinar, consectetur scelerisque quam. Suspendisse sed quam ac ligula porttitor mattis. Quisque auctor ligula eleifend, laoreet enim vitae, varius nisl. Quisque nec nibh eleifend, pulvinar turpis eget, fringilla justo. <br><br>Duis aliquet arcu venenatis, dignissim justo id, lobortis tellus. Nam hendrerit tellus ut nulla tempor, sed congue orci elementum. Ut vitae justo porttitor orci lobortis auctor eget sit amet lorem. Aliquam lobortis efficitur cursus. Etiam non dui massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. <br><br>Praesent posuere egestas velit sit amet molestie. Nulla facilisi. Fusce porta, est sed scelerisque ultricies, quam sem pharetra nibh, vitae ultricies sapien nisl in mauris. In at nunc et sem lobortis consectetur. Nam volutpat vitae dolor vitae gravida. Nullam vitae aliquam orci, vitae sagittis felis. Duis pellentesque hendrerit felis, blandit iaculis dolor maximus sit amet. Nullam congue malesuada massa, ac lobortis dui placerat sit amet. Duis eu maximus enim. Integer blandit risus justo, vel tincidunt arcu cursus vel. <br><br>Donec dictum ac purus sed suscipit. Sed ultrices nunc eget metus pellentesque elementum. Fusce consectetur sagittis nulla, vitae rhoncus libero mattis nec. Maecenas posuere finibus tincidunt. Aenean nulla diam, feugiat vitae dolor eu, finibus vulputate metus.",
+        date: getTime(),
+        img: "/images/griselda.jpg",
+        url: "/griselda",
+    },
+]
